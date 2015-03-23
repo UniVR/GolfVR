48aec5daae0d1ce62a6a3a9c29ea77e6d6f39b29
ludo6577 <ludo6577@gmail.com>
2015-03-23 18:12:16 +0100

    First commit (cleaning)

---
+++ Assets/ballScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballScript : MonoBehaviour {
+
+	public Collider club;
+	public AudioSource audio;
+
+	public float yVelocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider != club)
+			return;
+
+		float velocity = yVelocity * collision.relativeVelocity.z;
+		var ball = GetComponent<Rigidbody> ();
+		ball.AddForce (0, velocity, 0);
+
+		// Play a sound if the coliding objects had a big impact.		
+		if (collision.relativeVelocity.magnitude > 2)
+			audio.Play();
+	}
+}
---
+++ Assets/lookAt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookAt : MonoBehaviour {
+
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(target);
+	}
+}
