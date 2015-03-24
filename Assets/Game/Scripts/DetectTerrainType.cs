731cac851bafa6358f8b575c093876e00b382267
ludo6577 <ludo6577@gmail.com>
2015-03-24 15:34:39 +0100

    Ball physics depending on terrain texture + Re-order project (Game: /Balls /Clubs /Scripts /Sounds and /Terrains)
    Former-commit-id: 2ce9e4bfa27ecbe03288079bd5e9f4c2ff610c9f [formerly 63f7db1d749a051cbc0710ba8a60921c70ae7164] [formerly 86067faf94315bd893c283015f31f7eba8e605c6 [formerly 94831a3f9b7b9f46805c123e66d43d8722fe1b99]]
    Former-commit-id: aaa73a670f11d3b5cfad9b48eb368eb93d998271
    Former-commit-id: 6194d4ea04dd9948ec8d9343be88d2d0914a1b72

---
+++ Assets/Game/Scripts/DetectTerrainType.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/* 
+ * Based on this answer: How can I perform some action based on the terrain texture currently under my player?
+ * http://answers.unity3d.com/questions/14998/how-can-i-perform-some-action-based-on-the-terrain.html
+ */
+
+[Serializable]
+public class TexturePhysics{
+	[SerializeField] public float drag;
+	[SerializeField] public float angularDrag;
+}
+
+[RequireComponent(typeof (Rigidbody))]
+[RequireComponent(typeof (Transform))]
+public class DetectTerrainType : MonoBehaviour {
+	
+	private int textureSize;
+
+	[Tooltip("Physics of each layer on the terrain")]
+	[SerializeField] public TexturePhysics[] TerrainTexturePhysics;
+	
+	[Tooltip("Visualize the current texture under the object (auto generated values)")]
+	[SerializeField] public float[] currentTextureLevel;
+
+
+	private Transform ballTransf;
+	private Rigidbody ballRigidBody;
+
+	private TerrainData terrainData;
+	private Vector3 terrainPos;
+
+
+	void Start () {
+		ballTransf = GetComponent<Transform>();
+		ballRigidBody = GetComponent<Rigidbody> ();
+
+		Terrain terrain = Terrain.activeTerrain;
+		terrainData = terrain.terrainData;
+		terrainPos = terrain.transform.position;
+		
+		textureSize = TerrainTexturePhysics.Length;
+		currentTextureLevel = new float[textureSize];
+	}
+
+	void Update () {
+		var ballPos = ballTransf.position;
+		var mapX = ((ballPos.x - terrainPos.x) / terrainData.size.x) *  terrainData.alphamapWidth;
+		var mapZ = ((ballPos.z - terrainPos.z) / terrainData.size.z) * terrainData.alphamapHeight;
+		var splatmapData = terrainData.GetAlphamaps((int)mapX, (int)mapZ, 1, 1); 
+
+		float drag = 0;
+		float angularDrag = 0;
+
+		for (int i=0; i<textureSize; i++) {
+			currentTextureLevel[i] = splatmapData[0,0,i];
+			drag += TerrainTexturePhysics[i].drag * currentTextureLevel[i];
+			angularDrag += TerrainTexturePhysics[i].angularDrag * currentTextureLevel[i];
+		}
+
+		ballRigidBody.drag = drag;
+		ballRigidBody.angularDrag = angularDrag;
+	}
+}
