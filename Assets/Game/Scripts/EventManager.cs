628069a472af0a2bc113c1d67ba34bde176a8c2b
ludo6577 <ludo6577@gmail.com>
2015-04-07 15:17:21 +0200

    Integration of Cardboard + begin of GUI
    Former-commit-id: 0c0cf2872bf95345d3ef37c4ed6ced227ff92b42
    Former-commit-id: 9f8894b171a13550b6454fc4253f985afa9e9d3a
    Former-commit-id: 099bee67c1e88de2f3c42df80581303d303aca85

---
+++ Assets/Game/Scripts/EventManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+	public void LoadFirstScene(){
+		Application.LoadLevel("GolfVR");
+	}
+
+	public void ExitGame(){
+		Application.Quit();
+	}
+}
