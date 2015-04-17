797ba89b7168e0c8b015da23b422d86597ce55b0
ludo6577 <ludo6577@gmail.com>
2015-04-17 18:29:31 +0200

    Player gui

---
+++ Assets/Game/Scripts/PlayerActions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActions : MonoBehaviour {
+
+	private Transform playerPos;
+
+	// Use this for initialization
+	void Start () {
+		playerPos = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TurnLeft(){
+		playerPos.Rotate (Vector3.up, 1);
+	}
+
+	public void TurnRight(){
+		playerPos.Rotate (Vector3.up, -1);
+	}
+}
