99c54beebced705b2e52328e2584f33c9160e599
ludo6577 <ludo6577@gmail.com>
2015-04-20 13:27:45 +0200

    Rotate around the ball

--- Assets/Game/Scripts/PlayerActions.cs
+++ Assets/Game/Scripts/PlayerActions.cs
@@ -1,25 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerActions : MonoBehaviour {
 
+	public GameObject rotateAround;
+
+	public Button buttonLeft;
+	public Button buttonRight;
+
+	public enum ActionRotation{
+		TurnLeft,
+		TurnRight,
+		None
+	}
+
+	private ActionRotation action;
 	private Transform playerPos;
+	private Color buttonsColor;
 
 	// Use this for initialization
 	void Start () {
 		playerPos = GetComponent<Transform> ();
+		action = ActionRotation.None;
+		buttonsColor = buttonLeft.image.color;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if(action==ActionRotation.TurnLeft)
+			playerPos.RotateAround(rotateAround.transform.position, Vector3.up, -1);
+		else if(action==ActionRotation.TurnRight)
+			playerPos.RotateAround(rotateAround.transform.position, Vector3.up, 1);
 	}
 
 	public void TurnLeft(){
-		playerPos.Rotate (Vector3.up, 1);
+		action = ActionRotation.TurnLeft;
+		buttonLeft.image.color =  new Color(buttonsColor.r-1f, buttonsColor.g, buttonsColor.b-1f); 
 	}
 
 	public void TurnRight(){
-		playerPos.Rotate (Vector3.up, -1);
+		action = ActionRotation.TurnRight;
+		buttonRight.image.color =  new Color(buttonsColor.r-1f, buttonsColor.g, buttonsColor.b-1f); 
+	}
+
+	public void StopTurn(){
+		action = ActionRotation.None;
+		buttonRight.image.color =  new Color(buttonsColor.r, buttonsColor.g, buttonsColor.b); 
+		buttonLeft.image.color =  new Color(buttonsColor.r, buttonsColor.g, buttonsColor.b); 
 	}
 }
