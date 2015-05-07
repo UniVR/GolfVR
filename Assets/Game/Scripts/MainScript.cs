04b7c3b6336d1ad61304486849360f90d34ad760
ludo6577 <ludo6577@gmail.com>
2015-05-07 14:19:17 +0200

    Big code refactoring ! (Everything in a single file)

--- Assets/Game/Scripts/MainScript.cs
+++ Assets/Game/Scripts/MainScript.cs
@@ -26,45 +26,55 @@ public class MainScript : MonoBehaviour {
 	public MovementState currentMovement;
 
 	/*
-	 * 	Public Player/Club/Ball
+	 * 	Public Properties
 	 */
-	public GameObject Player;
-	
+	public GameObject Player;	
 	public GameObject Club;
-
 	public GameObject Ball;
 
-	/*
-	 * public GUI
-	 */
+	// Global Clubs properties
+	public float velocityLoading;
+	public float velocityShooting;	
+	public float minAngle;
+	public float midAngle;
+	public float maxAngle;
+
+	//GUI
 	public GameObject buttonLeft;
 	public GameObject buttonRight;
-	public float moveVelocity;
+	public float moveToBallVelocity;
 
 	/*
-	 * Private properties of the player
+	 * Private properties
 	 */	
-	private Transform playerPos;
+	//Player
+	private Transform playerTransf;
 	private Vector3 playerOffsetWithBall;
 
-	/*
-	 * 	Private properties of the ball
-	 */
+	//Club
+	private ClubProperties clubProperties;
+	private Transform clubTransf;
+	private Quaternion clubDefaultRotation;
+	private float timeLoading;
+
+	//Ball
 	private Rigidbody ballRigidBody;
 	private MeshRenderer ballRenderer;
 	private Color ballOriginalColor;
 	private Color ballCurrentColor;
 	private bool ballIsWatched;
+	private bool ballIsShooted;
 
-	/*
-	 * 	Private properties of the GUI
-	 */
+	//GUI
 	private Button buttonLeftBtn;
 	private Button buttonRightBtn;
 	private Color buttonsColor;
-	private bool hideGui;
+	private bool isGuiVisible;
+	
 
-	// Use this for initialization
+	/*
+	 * Initialisation
+	 */
 	void Start () {
 		currentAction = ActionState.Idle;
 		currentMovement = MovementState.None;
@@ -72,16 +82,24 @@ public class MainScript : MonoBehaviour {
 		/*
 		 * Player
 		 */		
-		playerPos = Player.transform;
+		playerTransf = Player.transform;
 		playerOffsetWithBall = Player.transform.position - Ball.transform.position;
 
 		/*
-		 * Ball properties initialization
+		 * 	Club
+		 */
+		clubProperties = Club.GetComponent<ClubProperties> ();
+		clubTransf = Club.transform;
+		clubDefaultRotation = new Quaternion(clubTransf.localRotation.x, clubTransf.localRotation.y, clubTransf.localRotation.z, clubTransf.localRotation.w);
+
+		/*
+		 * Ball
 		 */
 		ballRigidBody = Ball.GetComponent<Rigidbody> ();
 		ballRenderer = Ball.GetComponent<MeshRenderer> ();
 		ballOriginalColor = ballCurrentColor = ballRenderer.material.color;
 		ballIsWatched = false;
+		ballIsShooted = false;
 
 		/*
 		 * GUI
@@ -89,66 +107,142 @@ public class MainScript : MonoBehaviour {
 		buttonLeftBtn = buttonLeft.GetComponent<Button> ();
 		buttonRightBtn = buttonRight.GetComponent<Button> ();
 		buttonsColor = buttonLeftBtn.image.color;
-		hideGui = false;
+		isGuiVisible = true;
 	}
 
-	void Update () {
-	
-		if (ballIsWatched) {
-			ballCurrentColor = new Color(ballCurrentColor.r += 0.05f, ballCurrentColor.g, ballCurrentColor.b);
-			ballRenderer.material.color = ballCurrentColor;
-		} else if(ballCurrentColor != ballOriginalColor){
-			ballCurrentColor = ballOriginalColor;
-			ballRenderer.material.color = ballCurrentColor;
-		}
 
-		/*
-		 * 	Rotation left/right
-		 */
-		if (currentMovement == MovementState.TurnLeft)
-		{
-			buttonLeftBtn.image.color = new Color (buttonsColor.r - 1f, buttonsColor.g, buttonsColor.b - 1f); 
-			playerPos.RotateAround (Ball.transform.position, Vector3.up, -1);
-		} 
-		else if (currentMovement == MovementState.TurnRight) 
-		{
-			playerPos.RotateAround (Ball.transform.position, Vector3.up, 1);
-			buttonRightBtn.image.color = new Color (buttonsColor.r - 1f, buttonsColor.g, buttonsColor.b - 1f); 
-		}
-		else
-		{
-			buttonRightBtn.image.color =  new Color(buttonsColor.r, buttonsColor.g, buttonsColor.b); 
-			buttonLeftBtn.image.color =  new Color(buttonsColor.r, buttonsColor.g, buttonsColor.b); 
-		}
-		
-		if (currentAction != ActionState.Idle || ballRigidBody.velocity.magnitude > 0.5f) 
-		{
-			currentMovement = MovementState.None;
-			hideGui = false;
-			buttonLeft.SetActive(hideGui);
-			buttonRight.SetActive(hideGui);
-		} 
-		else
-		{
-			hideGui = true;
-			buttonLeft.SetActive(hideGui);
-			buttonRight.SetActive(hideGui);
-		}
-
-	}
-
-	// Update is called once per frame
 	void FixedUpdate () {
-		
 		/*
-		 * 	Moving to the ball
+		 * 	Action
 		 */
-		if (currentMovement == MovementState.MoveToTheBall) {
-			float step = moveVelocity * Time.deltaTime;
-			Player.transform.position = Vector3.MoveTowards (Player.transform.position, Ball.transform.position + playerOffsetWithBall, step);
-			if(transform.position == Ball.transform.position + playerOffsetWithBall)
-				currentMovement = MovementState.MoveToTheBall;
-		}	
+		switch (currentAction) 
+		{
+			/*
+			 * Idle
+			 */
+			case ActionState.Idle:
+				if(!isGuiVisible)
+				{
+					isGuiVisible = true;
+					buttonLeft.SetActive(true);
+					buttonRight.SetActive(true);
+				}
+			break;
+
+			/*
+			 * Loading
+			 */
+			case ActionState.Loading:
+				if(clubTransf.localRotation.z < maxAngle)
+				{
+					timeLoading += Time.deltaTime;
+					clubTransf.Rotate (Vector3.down * Time.deltaTime * velocityLoading);
+				}
+				else
+				{
+					currentAction = ActionState.Loaded;
+				}
+				
+				if(isGuiVisible)
+				{
+					isGuiVisible = false;
+					buttonLeft.SetActive(false);
+					buttonRight.SetActive(false);
+					currentMovement = MovementState.None;
+				}
+				
+				ballCurrentColor = new Color(ballCurrentColor.r += 0.05f, ballCurrentColor.g, ballCurrentColor.b);
+				ballRenderer.material.color = ballCurrentColor;
+			break;
+
+			/*
+			 * Loaded
+			 */
+			case ActionState.Loaded:
+			break;
+
+			/*
+			 * Firing
+			 */
+			case ActionState.Firing:
+				if(clubTransf.localRotation.z > minAngle)
+				{
+					clubTransf.Rotate (-Vector3.down * Time.deltaTime * velocityShooting * timeLoading);
+					if(!ballIsShooted && clubTransf.localRotation.z < midAngle)							//Shoot now
+					{
+						ShootBall(timeLoading, playerTransf.eulerAngles.y);
+						ballIsShooted = true;
+					}
+					else if (clubTransf.localRotation.z <= minAngle)
+					{
+						currentAction = ActionState.Fired;
+						ballIsShooted = false;
+						timeLoading = 0;
+					}
+				}
+				
+				ballCurrentColor = ballOriginalColor;
+				ballRenderer.material.color = ballCurrentColor;
+			break;
+
+			/*
+			 * Fired
+			 */
+			case ActionState.Fired:
+				if(ballRigidBody.velocity.magnitude < 0.1f)
+				{
+					clubTransf.localRotation = Quaternion.RotateTowards(clubTransf.localRotation, clubDefaultRotation, 10f);
+					if(currentMovement != MovementState.MoveToTheBall)
+					{
+						currentMovement = MovementState.MoveToTheBall;
+					}
+					if(clubTransf.localRotation.z > midAngle)
+					{
+						currentAction = ActionState.Idle;
+					}
+				}
+			break;
+		}
+
+		/*
+		 * 	Movement
+		 */
+		switch (currentMovement)
+		{
+			/*
+			 * 	None
+			 */
+			case MovementState.None:
+				buttonRightBtn.image.color =  new Color(buttonsColor.r, buttonsColor.g, buttonsColor.b); 
+				buttonLeftBtn.image.color =  new Color(buttonsColor.r, buttonsColor.g, buttonsColor.b); 
+			break;
+
+			/*
+			 * Turn left
+			 */
+			case MovementState.TurnLeft:
+				buttonLeftBtn.image.color = new Color (buttonsColor.r - 1f, buttonsColor.g, buttonsColor.b - 1f); 
+				playerTransf.RotateAround (Ball.transform.position, Vector3.up, -1);
+			break;
+
+			/*
+			 * Turn right
+			 */
+			case MovementState.TurnRight:
+				playerTransf.RotateAround (Ball.transform.position, Vector3.up, 1);
+				buttonRightBtn.image.color = new Color (buttonsColor.r - 1f, buttonsColor.g, buttonsColor.b - 1f); 
+			break;
+
+			/*
+			 * Move toward the ball
+			 */
+			case MovementState.MoveToTheBall:
+				float step = moveToBallVelocity * Time.deltaTime;
+				Player.transform.position = Vector3.MoveTowards (Player.transform.position, Ball.transform.position + playerOffsetWithBall, step);
+				if(transform.position == Ball.transform.position + playerOffsetWithBall)
+					currentMovement = MovementState.MoveToTheBall;
+			break;
+		}
 	}
 
 
@@ -174,6 +268,13 @@ public class MainScript : MonoBehaviour {
 		 * TODO: Sounds
 		 */
 	}
+
+	public void ShootBall (float magnitude, float orientation)
+	{
+		Vector3 direction = Quaternion.Euler(0, orientation, -clubProperties.clubAngle) * new Vector3 (-1, 0, 0);
+		ballRigidBody.AddForce(direction * magnitude * clubProperties.clubForceCoef, ForceMode.Impulse);	
+	}
+
 
 	/*
 	 * Button Left/Right
