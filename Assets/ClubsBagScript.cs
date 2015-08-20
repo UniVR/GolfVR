5cd532fbec5d988a8ae75d8d5d96a092816084d7
ludo6577 <ludo6577@gmail.com>
2015-08-20 13:15:36 +0200

    ClubScript

---
+++ Assets/ClubsBagScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ClubsBagScript : MonoBehaviour {
+
+	private string ButtonAsset = "ClubSelectionButton";
+
+	public Canvas Menu;
+	public List<GameObject> Clubs;
+
+	private List<Button> buttons;
+
+	void Start(){
+		foreach (GameObject club in Clubs) {
+			var ressource = Resources.Load<Button>(ButtonAsset);
+			Debug.Log(ButtonAsset);
+			Button clubButton = (Button)GameObject.Instantiate(ressource);
+			clubButton.transform.SetParent(Menu.transform, false);
+			//clubButton.onClick.AddListener(delegate() {/* StartGame("Level1"); */});
+			buttons.Add(clubButton);
+		}
+	}
+
+	public void OpenMenu(){
+
+	}
+
+	public void CloseMenu(){
+
+	}
+
+}
