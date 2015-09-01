e084f33f083bd48ac47ff163e6c53b7f815c450b
ludo6577 <ludo6577@gmail.com>
2015-09-01 13:53:51 +0200

    https://github.com/UniVR/GolfVR/issues/39 Part 2: Fix

--- Assets/Game/Scripts/CardboardScript.cs
+++ Assets/Game/Scripts/CardboardScript.cs
@@ -6,7 +6,7 @@ public class CardboardScript : MonoBehaviour {
 	private PlayerScript player;
 
 	private bool lookDown;
-	private Transform lastTransf;
+	private float lastRotation;
 
 	void Start () {
 		player = MainScript.Get ().Player;
@@ -17,7 +17,7 @@ public class CardboardScript : MonoBehaviour {
 	 * 	Rotation system
 	 */
 	void Update () {
-
+		return;
 		var headRotation = Cardboard.SDK.HeadPose.Orientation.eulerAngles;				// Head rotation
 		
 		var forwardRotationThresholdMin = 10; 											// Player look in direction of the ground/ball
@@ -27,14 +27,20 @@ public class CardboardScript : MonoBehaviour {
 		var lookHorizontally = headRotation.x < forwardRotationThresholdMin || headRotation.x > forwardRotationThresholdMax;
 		if (lookHorizontally) {
 			player.transform.eulerAngles = new Vector3 (0, headRotation.y, 0);
+			if(lookDown){
+				lookDown = false;
+				transform.eulerAngles = new Vector3 (headRotation.x, lastRotation, headRotation.z);
+			}
 			transform.eulerAngles = new Vector3 (headRotation.x, transform.eulerAngles.y, headRotation.z);
 		}  																				// Player look at the ground we follow his neck direction (Horizontal projection of the neck vector)
 		else { 
+			lookDown= true;
 			var neckVector = Cardboard.SDK.HeadPose.Orientation * Vector3.up;			// Neck vector
 			var direction = new Vector3 (neckVector.x, 0, neckVector.z);
 
 			player.transform.rotation = Quaternion.LookRotation (direction); 			// TODO: add a little threshold?
 			transform.eulerAngles = new Vector3 (headRotation.x, headRotation.y, headRotation.z);
+			lastRotation = headRotation.y;
 		}
 	}
 }
