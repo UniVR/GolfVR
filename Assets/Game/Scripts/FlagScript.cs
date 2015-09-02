a39d52e4c00c2e4bd28ec19291333580ae8787e5
ludo6577 <ludo6577@gmail.com>
2015-09-02 21:14:31 +0200

    Begin of GUI on top of the flag https://github.com/UniVR/GolfVR/issues/41

---
+++ Assets/Game/Scripts/FlagScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FlagScript : MonoBehaviour {
+
+	private BallScript ball;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		ball = MainScript.Get ().Ball;
+		text = GetComponentInChildren<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(text==null)
+			text = GetComponentInChildren<Text> ();
+
+		float distance = Vector3.Distance(transform.position, ball.transform.position);
+		distance = 3.00f;
+		distance += 0.001f;
+		text.text = "Distance:\n" + distance.ToString("0.00") + "m";
+	}
+}
