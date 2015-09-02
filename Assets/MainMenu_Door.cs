0acf9abb55a9833b7a77b7f00db14d27870e05c6
ludo6577 <ludo6577@gmail.com>
2015-09-02 14:20:11 +0200

    Add animator to the first scene

---
+++ Assets/MainMenu_Door.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu_Door : MonoBehaviour {
+
+	Animator animator;
+	int openHash = Animator.StringToHash ("Open");
+	int idleHash = Animator.StringToHash ("Idle");
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool open = Input.GetKey (KeyCode.Space);
+		if(open)
+			animator.SetTrigger ("Close");
+		if (open) {
+			//animator.SetTrigger (openHash);
+			animator.SetBool ("DoOpen", true);
+		}// else {
+	//		animator.SetTrigger (idleHash);
+	//	}
+
+		//Get the current state
+		AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo (0);
+	}
+}
