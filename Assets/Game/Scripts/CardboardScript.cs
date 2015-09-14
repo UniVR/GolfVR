29aa81e2f84e0822f451381199c2828b7d2e9369
ludo6577 <ludo6577@gmail.com>
2015-09-14 22:33:28 +0200

    Sound for club hit + smooth on cardboard

--- Assets/Game/Scripts/CardboardScript.cs
+++ Assets/Game/Scripts/CardboardScript.cs
@@ -5,7 +5,9 @@ public class CardboardScript : MonoBehaviour {
 
 	public float SmoothFactor;
 	public float ForwardRotationThresholdMin;
-	public float ForwardRotationThresholdMax; 	
+	public float ForwardRotationThresholdMax; 
+
+	public bool OldSystem;
 
 	private PlayerScript player;
 	private bool lookDown;
@@ -20,57 +22,60 @@ public class CardboardScript : MonoBehaviour {
 	 * 	Rotation system
 	 */
 	void Update () {
-		var headRotation = Cardboard.SDK.HeadPose.Orientation.eulerAngles;				// Head rotation			
-		var neckVector = Cardboard.SDK.HeadPose.Orientation * Vector3.up;			// Neck vector
 
-		var direction = new Vector3 (neckVector.x, 0, neckVector.z);
-		//Debug.Log("direction" + direction + "; Headrotation: " + headRotation);
-		
-		var lookHorizontally = headRotation.x < ForwardRotationThresholdMin || headRotation.x > ForwardRotationThresholdMax;
-		if (!lookHorizontally) {
-			lookDown = true;
-			var playerTarget = Quaternion.LookRotation (direction);
-			player.transform.rotation = Quaternion.Slerp(player.transform.rotation, playerTarget, Time.deltaTime * SmoothFactor);
-		} else {
-			if(lookDown){ //Fix the jump glitch
-				lookDown = false;
-				transform.eulerAngles = new Vector3 (headRotation.x, lastRotation, headRotation.z);
-			}
-			player.transform.rotation = Quaternion.Slerp(player.transform.rotation, Quaternion.Euler (0, headRotation.y, 0), Time.deltaTime * SmoothFactor);
-		}
-
-		var target = Quaternion.Euler (headRotation.x, headRotation.y, headRotation.z);
-		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * SmoothFactor);
-		lastRotation = transform.rotation.y;
-		//transform.eulerAngles = new Vector3 (headRotation.x, headRotation.y, headRotation.z);
-
-		return;
-		/*
-		 * 	OLD SYSTEM
-		 *
-		var headRotation = Cardboard.SDK.HeadPose.Orientation.eulerAngles;				// Head rotation
-		var forwardRotationThresholdMin = 10; 											// Player look in direction of the ground/ball
-		var forwardRotationThresholdMax = 90; 	
-
-		// Player look at the horizon (normal rotation around the ball)
-		var lookHorizontally = headRotation.x < forwardRotationThresholdMin || headRotation.x > forwardRotationThresholdMax;
-		if (lookHorizontally) {
-			player.transform.eulerAngles = new Vector3 (0, headRotation.y, 0);
-			if(lookDown){ //Fix the jump glitch
-				lookDown = false;
-				transform.eulerAngles = new Vector3 (headRotation.x, lastRotation, headRotation.z);
-			}
-
-			transform.eulerAngles = new Vector3 (headRotation.x, transform.eulerAngles.y, headRotation.z);
-		}  																				// Player look at the ground we follow his neck direction (Horizontal projection of the neck vector)
-		else { 
-			lookDown= true;
+		if (!OldSystem) {
+			var headRotation = Cardboard.SDK.HeadPose.Orientation.eulerAngles;				// Head rotation			
 			var neckVector = Cardboard.SDK.HeadPose.Orientation * Vector3.up;			// Neck vector
-			var direction = new Vector3 (neckVector.x, 0, neckVector.z);
 
-			player.transform.rotation = Quaternion.LookRotation (direction); 			// TODO: add a little threshold?
-			transform.eulerAngles = new Vector3 (headRotation.x, headRotation.y, headRotation.z);
-			lastRotation = headRotation.y;
-		}*/
+			var direction = new Vector3 (neckVector.x, 0, neckVector.z);
+			//Debug.Log("direction" + direction + "; Headrotation: " + headRotation);
+		
+			var lookHorizontally = headRotation.x < ForwardRotationThresholdMin || headRotation.x > ForwardRotationThresholdMax;
+			if (!lookHorizontally) {
+				lookDown = true;
+				var playerTarget = Quaternion.LookRotation (direction);
+				player.transform.rotation = Quaternion.Slerp (player.transform.rotation, playerTarget, Time.deltaTime * SmoothFactor);
+			} else {
+				if (lookDown) { //Fix the jump glitch
+					lookDown = false;
+					transform.eulerAngles = new Vector3 (headRotation.x, lastRotation, headRotation.z);
+				}
+				player.transform.rotation = Quaternion.Slerp (player.transform.rotation, Quaternion.Euler (0, headRotation.y, 0), Time.deltaTime * SmoothFactor);
+			}
+
+			var target = Quaternion.Euler (headRotation.x, headRotation.y, headRotation.z);
+			transform.rotation = Quaternion.Slerp (transform.rotation, target, Time.deltaTime * SmoothFactor);
+			lastRotation = transform.rotation.y;
+			//transform.eulerAngles = new Vector3 (headRotation.x, headRotation.y, headRotation.z);
+
+		}else{
+			/*
+			 * 	OLD SYSTEM
+			 */
+			var headRotation = Cardboard.SDK.HeadPose.Orientation.eulerAngles;				// Head rotation
+			var forwardRotationThresholdMin = 10; 											// Player look in direction of the ground/ball
+			var forwardRotationThresholdMax = 90; 	
+
+			// Player look at the horizon (normal rotation around the ball)
+			var lookHorizontally = headRotation.x < forwardRotationThresholdMin || headRotation.x > forwardRotationThresholdMax;
+			if (lookHorizontally) {
+				player.transform.eulerAngles = new Vector3 (0, headRotation.y, 0);
+				if(lookDown){ //Fix the jump glitch
+					lookDown = false;
+					transform.eulerAngles = new Vector3 (headRotation.x, lastRotation, headRotation.z);
+				}
+
+				transform.eulerAngles = new Vector3 (headRotation.x, transform.eulerAngles.y, headRotation.z);
+			}  																				// Player look at the ground we follow his neck direction (Horizontal projection of the neck vector)
+			else { 
+				lookDown= true;
+				var neckVector = Cardboard.SDK.HeadPose.Orientation * Vector3.up;			// Neck vector
+				var direction = new Vector3 (neckVector.x, 0, neckVector.z);
+
+				player.transform.rotation = Quaternion.LookRotation (direction); 			// TODO: add a little threshold?
+				transform.eulerAngles = new Vector3 (headRotation.x, headRotation.y, headRotation.z);
+				lastRotation = headRotation.y;
+			}
+		}
 	}
 }
