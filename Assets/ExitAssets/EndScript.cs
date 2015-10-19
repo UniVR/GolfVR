276fedef2dd0e54b21f7f236f0cf04674c69da0e
MattGen <magenoud@gmail.com>
2015-10-19 07:43:13 +0200

    Exit/Restart/Share Menu

---
+++ Assets/ExitAssets/EndScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void likeUs () {
+		Application.OpenURL ("https://www.facebook.com/UniVR.GolfVR");
+	}
+
+	void followUs () {
+		Application.OpenURL ("https://twitter.com/UniGolfVR");
+	}
+
+	void restart () {
+		Application.LoadLevel ("MainMenu");
+	}
+
+	void quit () {
+		Application.Quit ();
+	}
+}
