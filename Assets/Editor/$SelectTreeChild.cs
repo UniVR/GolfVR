af29c0026c2276540b4716dcc566fd05d34b9024
MattGen <magenoud@gmail.com>
2016-01-04 21:52:09 +0100

    Trees placement is over !

--- Assets/Editor/$SelectTreeChild.cs
+++ Assets/Editor/$SelectTreeChild.cs
@@ -6,11 +6,11 @@ using System.Collections.Generic;
 public class SelectTreeChild {
 
 	[MenuItem ("Terrain/Select TreeChild")]
-	public static void SelectTreeChilds () {
+	public static void DoSelectTreeChild () {
 		GameObject[] objs = Selection.gameObjects;
 		List<GameObject> childrens = new List<GameObject>();
 		foreach (GameObject obj in objs) {
-			if (obj.gameObject.tag == "TreeChild") {
+			if (obj.gameObject.name == "Tree2") {
 				childrens.Add(obj.transform.gameObject);
 			}
 		}
